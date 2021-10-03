288619fd0435f5385725eac94b02e7341c75136b
Arcoz0308 <legrandroi33@gmail.com>
2021-10-03 16:53:06 +0200

    more performed linecounter

--- utils/logger/log_manager.go
+++ utils/logger/log_manager.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"github.com/arcoz0308/arcoz0308.tech/utils"
@@ -122,20 +123,31 @@ func RequestLog(msg string) {
 }
 
 func LineCounter(r io.Reader) (int, error) {
-	buf := make([]byte, 32*1024)
-	count := 0
-	lineSep := []byte{'\n'}
+
+	var count int
+	const lineBreak = '\n'
+
+	buf := make([]byte, bufio.MaxScanTokenSize)
 
 	for {
-		c, err := r.Read(buf)
-		count += bytes.Count(buf[:c], lineSep)
+		bufferSize, err := r.Read(buf)
+		if err != nil && err != io.EOF {
+			return 0, err
+		}
 
-		switch {
-		case err == io.EOF:
-			return count, nil
-
-		case err != nil:
-			return count, err
+		var buffPosition int
+		for {
+			i := bytes.IndexByte(buf[buffPosition:], lineBreak)
+			if i == -1 || bufferSize == buffPosition {
+				break
+			}
+			buffPosition += i + 1
+			count++
+		}
+		if err == io.EOF {
+			break
 		}
 	}
+
+	return count, nil
 }
